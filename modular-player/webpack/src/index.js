3bfbdcd28512133721f5e5bf01b279c97c9918f8
Martin Fillafer <martin.fillafer@bitmovin.com>
2018-11-05 15:29:24 +0100

    Add webpack modular-player sample

---
+++ modular-player/webpack/src/index.js
@@ -0,0 +1,42 @@
+import { Player } from 'bitmovin-player/modules/bitmovinplayer-core';
+import EngineBitmovinModule from 'bitmovin-player/modules/bitmovinplayer-engine-bitmovin';
+import MseRendererModule from 'bitmovin-player/modules/bitmovinplayer-mserenderer';
+import HlsModule from 'bitmovin-player/modules/bitmovinplayer-hls';
+import AbrModule from 'bitmovin-player/modules/bitmovinplayer-abr';
+import ContainerTSModule from 'bitmovin-player/modules/bitmovinplayer-container-ts';
+import SubtitlesModule from 'bitmovin-player/modules/bitmovinplayer-subtitles';
+import SubtitlesCEA608Module from 'bitmovin-player/modules/bitmovinplayer-subtitles-cea608';
+import PolyfillModule from 'bitmovin-player/modules/bitmovinplayer-polyfill';
+import StyleModule from 'bitmovin-player/modules/bitmovinplayer-style';
+
+import { UIFactory } from 'bitmovin-player-ui';
+import 'bitmovin-player-ui/dist/css/bitmovinplayer-ui.min.css';
+
+Player.addModule(EngineBitmovinModule);
+Player.addModule(MseRendererModule);
+Player.addModule(HlsModule);
+Player.addModule(AbrModule);
+Player.addModule(ContainerTSModule);
+Player.addModule(SubtitlesModule);
+Player.addModule(SubtitlesCEA608Module);
+Player.addModule(PolyfillModule);
+Player.addModule(StyleModule);
+
+const conf = {
+    key: 'YOUR KEY HERE',
+    ui: false
+};
+const source = {
+    hls: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
+    poster: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/poster.jpg'
+};
+
+const player = new Player(document.getElementById('player'), conf);
+
+UIFactory.buildDefaultUI(player);
+
+player.load(source).then(() => {
+    console.log('Successfully loaded source');
+}, (error) => {
+    console.log('Error while loading source', error);
+});
---
+++ modular-player/webpack/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: [
+          path.resolve(__dirname),
+          path.join(__dirname, '../..')
+        ],
+        publicPath: '/dist/',
+        host: '127.0.0.1',
+        port: 3035
+    }
+};
