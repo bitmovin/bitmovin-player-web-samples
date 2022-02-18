cbe3721611a142751c3d39068657b682e4d178d6
dani <dan.yaniv@bitmovin.com>
2022-02-18 14:43:57 -0500

    minor debug errors fixed in ads tutorial examples; replace `var` with `const` and `let`

--- tutorials_examples/ads_intro/schedule_adbreak_after_specific_time_of_playback/script.js
+++ tutorials_examples/ads_intro/schedule_adbreak_after_specific_time_of_playback/script.js
@@ -1,43 +1,48 @@
-var playerConfig = {
+const playerConfig = {
   // this license key is only for the tutorial examples
   key: "29ba4a30-8b5e-4336-a7dd-c94ff3b25f30",
-  advertising: { }
-};
-
-const source = {
-  "dash": "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd",
-  "hls": "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
-  "smooth": "https://test.playready.microsoft.com/smoothstreaming/SSWSS720H264/SuperSpeedway_720.ism/manifest",
-  "progressive": "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/MI201109210084_mpeg-4_hd_high_1080p25_10mbits.mp4",
-  "poster": "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/poster.jpg"
+  advertising: {}
 };
 
 // By default Bitmovin Player uses IMA Advertising Module so this activates Bitmovin Advertising Module to give you more control
 bitmovin.player.Player.addModule(window.bitmovin.player['advertising-bitmovin'].default);
 
-var player = new bitmovin.player.Player(document.getElementById('player'), playerConfig);
+const player = new bitmovin.player.Player(document.getElementById('player'), playerConfig);
 
-player.load(source);
+player.load(getSource())
+.then( () => {
+  // Observe player `TimeChanged` event and schedule the immediate `AdBreak` when it reached the threshold
+  let timePlayed = 0;
+  player.on('timechanged', function(event) {
+    if (timePlayed >= 15) {
+      scheduleImmediateAdBreak();
+      timePlayed = 0;
+    } else {
+      timePlayed = event.time;
+    }
+  });
+}).catch( (err) => console.log("[!]", err) );
 
 function scheduleImmediateAdBreak() {
   // Current playback position of the main content
-  var currentTime = player.getCurrentTime();
+  const currentTime = player.getCurrentTime();
   player.ads.schedule({
     tag: {
       url: 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=',
       type: 'vast'
     },
     id: 'Ad',
-    position: currentTime,
+    position: currentTime.toString(),
   });
 }
-// Observe player `TimeChanged` event and schedule the immediate `AdBreak` when it reached the threshold
-var timePlayed = 0;
-player.on('timechanged', function(event) {
-  if (counter >= 15) {
-    scheduleImmediateAdBreak();
-    counter = 0;
-  } else {
-    counter = event.time;
-  }
-});
+
+function getSource()
+{
+  return ({
+    "dash": "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd",
+    "hls": "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
+    "smooth": "https://test.playready.microsoft.com/smoothstreaming/SSWSS720H264/SuperSpeedway_720.ism/manifest",
+    "progressive": "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/MI201109210084_mpeg-4_hd_high_1080p25_10mbits.mp4",
+    "poster": "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/poster.jpg"
+  });
+}
--- tutorials_examples/ads_intro/schedule_immediate_adbreak/script.js
+++ tutorials_examples/ads_intro/schedule_immediate_adbreak/script.js
@@ -1,44 +1,47 @@
-var playerConfig = {
+const playerConfig = {
   // this license key is only for the tutorial examples
   key: "29ba4a30-8b5e-4336-a7dd-c94ff3b25f30",
   advertising: {}
 };
 
-const source = {
-  "dash": "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd",
-  "hls": "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
-  "smooth": "https://test.playready.microsoft.com/smoothstreaming/SSWSS720H264/SuperSpeedway_720.ism/manifest",
-  "progressive": "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/MI201109210084_mpeg-4_hd_high_1080p25_10mbits.mp4",
-  "poster": "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/poster.jpg"
-};
-
 // By default Bitmovin Player uses IMA Advertising Module so this activates Bitmovin Advertising Module to give you more control
 bitmovin.player.Player.addModule(window.bitmovin.player['advertising-bitmovin'].default);
 
-var player = new bitmovin.player.Player(document.getElementById('player'), playerConfig);
+const player = new bitmovin.player.Player(document.getElementById('player'), playerConfig);
 
-player.load(source);
+player.load(getSource());
 
 function scheduleImmediateAdBreak() {
   // Current playback position of the main content
-  var currentTime = player.getCurrentTime();
+  const currentTime = player.getCurrentTime();
   player.ads.schedule({
     tag: {
       url: 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=',
       type: 'vast'
     },
     id: 'Ad',
-    position: currentTime,
+    position: currentTime.toString(),
   });
 }
 
 // Use a button to schedule an ad whenever its clicked
-var scheduleBtn = document.getElementById('scheduleBtn');
-skipBtn.addEventListener('click', function () {
+const scheduleBtn = document.getElementById('scheduleBtn');
+scheduleBtn.addEventListener('click', function () {
   scheduleImmediateAdBreak();
 });
 // Use this later in the code to let the user skip the current ad
-var skipBtn = document.getElementById('skipButton');
+const skipBtn = document.getElementById('skipBtn');
 skipBtn.addEventListener('click', function () {
   player.ads.skip();
 });
+
+function getSource()
+{
+  return ({
+    "dash": "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd",
+    "hls": "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
+    "smooth": "https://test.playready.microsoft.com/smoothstreaming/SSWSS720H264/SuperSpeedway_720.ism/manifest",
+    "progressive": "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/MI201109210084_mpeg-4_hd_high_1080p25_10mbits.mp4",
+    "poster": "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/poster.jpg"
+  });
+}
