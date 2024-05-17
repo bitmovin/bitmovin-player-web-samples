d4c7594e57e77e15cc9297b72368066239e4a5db
Kirill Karpov <karpov.kir.v@gmail.com>
2024-05-17 15:37:43 +0400

    Migrate to React Conviva sample to additionalStandardTags

--- react/bundled-player/package-based-with-conviva/src/App.tsx
+++ react/bundled-player/package-based-with-conviva/src/App.tsx
@@ -1,19 +1,19 @@
-import ConvivaAnalyticsModule from '@bitmovin/player-integration-conviva';
-import { PlayerConfig, PlayerEvent, SourceConfig } from 'bitmovin-player';
+import { ConvivaAnalytics } from '@bitmovin/player-integration-conviva';
+import { AdTagConfig, PlayerConfig, PlayerEvent, SourceConfig } from 'bitmovin-player';
 import { BitmovinPlayer } from 'bitmovin-player-react';
 import { Fragment } from 'react';
 
 const defaultPlayerSource: SourceConfig = {
   hls: 'https://cdn.bitmovin.com/content/assets/streams-sample-video/sintel/m3u8/index.m3u8',
+  // Live example
+  // dash: 'https://cmafref.akamaized.net/cmaf/live-ull/2006350/akambr/out.mpd'
 };
-
-const {ConvivaAnalytics} = ConvivaAnalyticsModule;
 
 const playerConfig: PlayerConfig = {
   key: '<playerKey>',
   playback: {
     muted: true,
-    autoplay: true,
+    autoplay: false,
   },
   advertising: {
     adBreaks: [
@@ -24,7 +24,7 @@ const playerConfig: PlayerConfig = {
         },
         id: 'ad-pre-skippable',
         position: 'pre',
-      } as any,
+      } as AdTagConfig,
       {
         tag: {
           url: 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/32573358/skippable_ad_unit&impl=s&gdfp_req=1&env=vp&output=xml_vast2&unviewed_position_start=1&url=http%3A%2F%2Freleasetest.dash-player.com%2Fads%2F&description_url=[description_url]&correlator=[random]',
@@ -32,7 +32,7 @@ const playerConfig: PlayerConfig = {
         },
         id: 'ad-30-skippable',
         position: '30%',
-      } as any,
+      } as AdTagConfig,
       {
         tag: {
           url: 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/32573358/2nd_test_ad_unit&ciu_szs=300x100&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&url=[referrer_url]&description_url=[description_url]&correlator=[random]',
@@ -40,7 +40,7 @@ const playerConfig: PlayerConfig = {
         },
         id: 'ad-post-skippable',
         position: 'post',
-      } as any
+      } as AdTagConfig
     ]
   }
 };
@@ -63,26 +63,32 @@ export function App() {
               return;
             }
 
-            const convivaAnalytics = new ConvivaAnalytics(player as any, '<convivaKey>', {
+            const convivaAnalytics = new ConvivaAnalytics(player, '<convivaKey>', {
               debugLoggingEnabled: true,
-
               // It's used for testing and should not be used in production.
-              gatewayUrl: 'https://conviva-learningcenter.testonly.conviva.com'
+              gatewayUrl: 'https://conviva-learningcenter.testonly.conviva.com',
+              // This is inferred automatically, but can be overridden.
+              // deviceMetadata: { ... }
             });
 
             // Initialize metadata. It also can be used to update some data during playback.
             convivaAnalytics.updateContentMetadata({
-              viewerId: 'bitmovin-player-web-conviva-integration-viewer',
-              applicationName: 'Bitmovin Player Web Conviva Analytics Integration',
+              viewerId: 'bitmovin-player-web-conviva-integration-viewer-7',
+              // It's also referred as "Player name" on Conviva in some places.
+              applicationName: 'Bitmovin Web Player',
               assetName: 'Sintel',
+              defaultResource: 'TEST-DEFAULT-RESOURCE',
               custom: {
-                assetId: 'assetId',
                 directors: 'Colin Levy',
                 year: '2010',
-                playerVendor: 'bitmovin',
-                playerVersion: player.version,
-
-                // Reserved Conviva tags can be placed here.
+              },
+              // Reserved Conviva tags can be placed here.
+              // Find the list:
+              // - Here https://pulse.conviva.com/app/appmanager/meta-data (Required Metadata tab)
+              // - Or here https://pulse.conviva.com/learning-center/content/sensor_developer_center/sensor_integration/javascript/js_quick_integration.htm
+              //   under "Constants for Pre-defined Video and Content Metadata"
+              additionalStandardTags: {
+                'c3.app.version': '0.0.1',
                 'c3.cm.contentType': 'VOD',
                 'c3.cm.channel': 'Test Channel',
                 'c3.cm.brand': 'Test Brand',
@@ -96,7 +102,12 @@ export function App() {
                 'c3.cm.episodeNumber': 'Test Episode Number',
                 'c3.cm.genre': 'Test Primary Genre',
                 'c3.cm.genreList': 'Test Genre List Item 1, Test Genre List Item 2',
-              }
+                'c3.cm.utmTrackingUrl': 'https://test-utm-tracking-url',
+                // Cannot be determined reliably, but if there is a need this approximation can be used.
+                dcType: (navigator as Navigator & { connection?: { type: string } }).connection?.type
+                  || (navigator as Navigator & { connection?: { effectiveType: string } }).connection?.effectiveType
+                  || 'NA'
+              },
             });
 
             player.on(PlayerEvent.Destroy, () => {
--- react/bundled-player/package-based-with-conviva/vite.config.ts
+++ react/bundled-player/package-based-with-conviva/vite.config.ts
@@ -1,7 +1,8 @@
 import react from '@vitejs/plugin-react';
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteCommonjs()],
 });
