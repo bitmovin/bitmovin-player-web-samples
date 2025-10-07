864941f37ad89a3baa13abdb967564bc1741fc6a
Daniel Weinberger <dweinber@users.noreply.github.com>
2025-10-07 13:49:58 +0200

    Exchange unavailable asset with new one

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -2,18 +2,18 @@ import { Player } from 'bitmovin-player';
 import { UIFactory } from 'bitmovin-player-ui';
 require('bitmovin-player-ui/dist/css/bitmovinplayer-ui.min.css');
 
-var conf = {
-    key: 'YOUR KEY HERE',
-    ui: false
+const conf = {
+  key: 'YOUR KEY HERE',
+  ui: false
 };
-var source = {
-    dash: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd',
-    hls: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
-    progressive: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/MI201109210084_mpeg-4_hd_high_1080p25_10mbits.mp4',
-    poster: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/poster.jpg'
+const source = {
+  dash: 'https://cdn.bitmovin.com/content/assets/art-of-motion-dash-hls-progressive/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd',
+  hls: 'https://cdn.bitmovin.com/content/assets/art-of-motion-dash-hls-progressive/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
+  progressive: 'https://cdn.bitmovin.com/content/assets/art-of-motion-dash-hls-progressive/MI201109210084_mpeg-4_hd_high_1080p25_10mbits.mp4',
+  poster: 'https://cdn.bitmovin.com/content/assets/art-of-motion-dash-hls-progressive/poster.jpg'
 };
 
-var player = new Player(document.getElementById('player'), conf);
+const player = new Player(document.getElementById('player'), conf);
 
 UIFactory.buildDefaultUI(player);
 player.load(source).then(function () {
